9f44d17f276a3f9ad2e2f9a63b9bb23d30180115
joshi <joshi@DESKTOP-R6FEH3V.fritz.box>
2019-06-09 20:54:11 +0200

    Produkt create() + update () added

--- HighSpeed-Procurement/Source Packages/model/Produkt/Produkt.java
+++ HighSpeed-Procurement/Source Packages/model/Produkt/Produkt.java
@@ -217,14 +217,68 @@ private static Connection connection=null;
 	
 	public void writeAll(ArrayList<Produkt> produkte) {
 		//Auf der Tabelle auf dem Layer alle Produkte darstellen
+		//vlt zum überschreiben gedacht ?
 	}
 	
 	public void create() {
 		//Ein Produkt in der DB speichern
+		
+		//uberprufenfen ob readOneLayer() Daten erheben konnte				
+		if (!(this == null)) {
+			//string für die Datenbank bauen
+			String string =this.objectToString();		
+
+					try {
+						// Datenbank speichern
+						Statement stmt = connection.createStatement();
+						stmt.executeUpdate("INSERT INTO PRODUKTE (TYP, NAME, HERSTELLER, STRING)"
+								+ " VALUES " + "('" + this.getTyp()+ "','"+this.getName()+ "';'"+this.getHersteller()+ "';'"+string+ "'");
+
+					} catch (SQLException e) {
+						// Meldung Lieferant speichern fehlgeschlagen
+						Alert alert = new Alert(AlertType.ERROR);
+						alert.setTitle("Error Dialog: Lieferant.create()");
+						alert.setHeaderText("Folgender Fehler ist aufgetreten:");
+						alert.setContentText("Speichern in der Datenbank fehlgeschlagen!");
+						return;
+					}
+					// Meldung Lieferant erfolgreich gespeichert
+					Alert alert = new Alert(AlertType.INFORMATION);
+					alert.setTitle("Information Dialog: Lieferant.create()");
+					alert.setHeaderText("Folgendes Ereignis ist eingetreten:");
+					alert.setContentText("Lieferant erfolgreich gespeichert!");
+				}
 	}
 	
 	public void update(int iD) {
 		//Einen bestimmtes bestehendes Produkt in der Datenbank durch neue Werte ersetzen
+		
+		// uberprufen ob readOneLayer() Daten erheben konnte
+				if (!(this == null)) {
+					//string für die Datenbank bauen
+					String string =this.objectToString();
+
+					try {
+						// In der Datenbank speichern
+						Statement stmt = connection.createStatement();
+
+						stmt.executeUpdate("Update LIEFERANTEN Set TYP ='" + this.getTyp() + "',NAME='"+this.getName()
+							+ "',HERSTELLER='"+this.getHersteller()+"',STRING='"+string+"' WHERE ID='" + iD + "'");
+								
+					} catch (SQLException e) {
+						// Meldung Lieferant speichern fehlgeschlagen
+						Alert alert = new Alert(AlertType.ERROR);
+						alert.setTitle("Error Dialog: Lieferant.create()");
+						alert.setHeaderText("Folgender Fehler ist aufgetreten:");
+						alert.setContentText("Speichern in der Datenbank fehlgeschlagen!");
+						return;
+					}
+					// Meldung Lieferant erfolgreich gespeichert
+					Alert alert = new Alert(AlertType.INFORMATION);
+					alert.setTitle("Information Dialog: Lieferant.create()");
+					alert.setHeaderText("Folgendes Ereignis ist eingetreten:");
+					alert.setContentText("Lieferant erfolgreich gespeichert!");
+				}
 	}
 	
 	public String objectToString() {
@@ -240,9 +294,4 @@ private static Connection connection=null;
 		return null;
 		
 	}
-
-	public static TextField [] getTextfelder() {
-		return Textfelder;
-	}
-
 }
