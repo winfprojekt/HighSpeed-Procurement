3018d616482de5ab7d680271b65a99385067b535
denislavkszv <denislavkszv@gmail.com>
2019-06-22 23:33:52 +0200

    Changed main.

--- HighSpeed-Procurement/source packages/main/Main.java
+++ HighSpeed-Procurement/source packages/main/Main.java
@@ -111,15 +111,7 @@ public class Main extends Application {
 
 	}
 
-	public static void main(String[] args) throws NoSuchAlgorithmException {
-		// create an Object from DBUtil-class
-		util.DBUtil db = new util.DBUtil();
-		// connect to the database "aj9s-einkauf"ferrefe
-		/*try {
-			db.connectToMysql("193.196.143.168", "aj9s-einkauf", "aj9s-einkauf", "DYUcen7KXKo3dPzt", "UTC");
-		} catch (ClassNotFoundException | SQLException e) {
-			e.printStackTrace();
-		}*/
+	public static void main(String[] args) {
 
 		launch(args);
 	}
